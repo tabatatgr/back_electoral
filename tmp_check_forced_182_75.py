82f1ab9a080e582eea625875b8c0f8b006c5dc2c
tabatatgr <tabata.garcia@transformaciondigital.gob.mx>
2025-09-17 15:18:32 -0600

    chore: remove outputs artifacts and zero-byte tmp scripts (can be regenerated)

--- tmp_check_forced_182_75.py
+++
@@ -1,7 +0,0 @@
-from engine.procesar_diputados_v2 import procesar_diputados_v2
-
-res = procesar_diputados_v2(path_parquet='data/computos_diputados_2024.parquet', anio=2024, path_siglado='data/siglado-diputados-2024.csv', max_seats=500, sistema='mixto', mr_seats=182, rp_seats=75, usar_coaliciones=True, print_debug=False)
-print('MR (top):', {k:v for k,v in sorted(res['mr'].items(), key=lambda x:-x[1])[:10]})
-print('RP (top):', {k:v for k,v in sorted(res['rp'].items(), key=lambda x:-x[1])[:10]})
-print('TOT (top):', {k:v for k,v in sorted(res['tot'].items(), key=lambda x:-x[1])[:10]})
-print('Sum TOT:', sum(res['tot'].values()))
--- tmp_run_one_scenario.py
+++
@@ -1,4 +0,0 @@
-from engine.procesar_diputados_v2 import procesar_diputados_v2
-import json
-res = procesar_diputados_v2(path_parquet='data/computos_diputados_2024.parquet', anio=2024, path_siglado='data/siglado-diputados-2024.normalized.csv', max_seats=500, sistema='mixto', mr_seats=300, rp_seats=200, usar_coaliciones=True, sobrerrepresentacion=8.0, umbral=0.03, print_debug=False)
-print(json.dumps({'mr':res.get('mr'), 'rp':res.get('rp'), 'tot':res.get('tot')}, ensure_ascii=False, indent=2))
